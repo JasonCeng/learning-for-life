abe16aab3a2db7cfb8922ab66b08272684ec8629
JasonCeng <489281085@qq.com>
2020-04-13 23:39:29 +0800

    add VaildParen.cpp

---
+++ Algorithms/Stack/VaildParen.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for(char& c : s) {
+            switch(c) {
+                case '(': paren.push(c); break;
+                case '[': paren.push(c); break;
+                case '{': paren.push(c); break;
+                case ')': if(paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case ']': if(paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                case '}': if(paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                default: break;
+            }
+        }
+        return paren.empty();
+    }
+};
