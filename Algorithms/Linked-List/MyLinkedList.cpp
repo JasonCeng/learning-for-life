e61d71b737f29ad5c74ab8618aa7f114167fe8e8
JasonCeng <489281085@qq.com>
2020-04-30 23:46:22 +0800

    add addAtIndex

--- Algorithms/Linked-List/MyLinkedList.cpp
+++ Algorithms/Linked-List/MyLinkedList.cpp
@@ -65,7 +65,29 @@ public:
 
   /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
   void addAtIndex(int index, int val) {
-
+    if(index > this->size) {
+      return;
+    }
+    if(index == this->size) {
+      this->addAtTail(val);
+      return;
+    }
+    if(index == 0) {
+      this->addAtHead(val);
+      return;
+    }
+    int count=0;
+    Node* temp = this->head;
+    while(temp) {
+      if(count++ == index-1) {
+        Node * mid = new Node(val);
+        mid->next = temp->next;
+        temp->next = mid;
+        break;
+      }
+      temp = temp->next;
+    }
+    this->size++;
   }
 
   /** Delete the index-th node in the linked list, if the index is valid. */
